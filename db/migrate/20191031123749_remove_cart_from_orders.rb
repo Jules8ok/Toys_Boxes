0e2c9ac890fac9444a5cb994052d5115075ae5a9
jules wittock <wittockjules@gmail.com>
2019-11-27 16:42:09 +0100

    test

--- db/migrate/20191031123749_remove_cart_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCartFromOrders < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :orders, :cart, null: true, foreign_key: true
-  end
-end
--- db/migrate/20191031124507_drop_carts_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCartsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :carts
-  end
-end
